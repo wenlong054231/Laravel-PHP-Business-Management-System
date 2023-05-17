e25bb630a03a82b1482d8fb563397e2f649785a8
wenlong054231 <wenlongchow@yahoo.com>
2023-05-18 04:19:40 +0800

    m-auto for center

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -24,14 +24,10 @@
 
 <body class="bg-gradient-primary">
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
-
+            <div class="col-xl-10 col-lg-12 col-md-9 m-auto">
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
-                       
-                           
-                           
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
