0f62a1a5ec4a115e1eccb82b2cd920e745752e9e
wenlong054231 <wenlongchow@yahoo.com>
2023-05-26 01:57:21 +0800

    newrole, encrypted pw

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,9 +19,10 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'phone' => fake()->phoneNumber(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
         ];
     }
@@ -34,5 +35,24 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    public function admin()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'role' => 'admin',
+            ];
+        });
+    }
+
+    
+    public function staff()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'role' => 'staff',
+            ];
+        });
     }
 }
